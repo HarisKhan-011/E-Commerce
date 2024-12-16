e39d85a9c719e0ade49c5414bf84209bb434e9d6
HarisKhan-011 <aharis0342@gmail.com>
2024-12-16 20:36:51 +0500

    Update Contactus.jsx

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Contactus.css';
 
 export const Contactus = () => {
+const [error,seterror]=useState('')
+const [formdata,setformdata]=useState({name:"",email:"",message:""})
 
+
+const handleinput=(e)=>{
+setformdata({...formdata,[e.target.name]:e.target.value})
+}
   const submitbutton=(e)=>{
-    if (e.name ===''){
-alert("Fill the foam")
-    }
-    alert("Your data has benn sumbit")
+  e.preventDefault();
+  if(formdata.name===""||formdata.email==="" ||formdata.message===""){
+    seterror("Please fill out all fields")
+  }else{
+    seterror("")
+    alert("Your data has been submited")
+    setformdata({name:"",email:"",message:""})
+  }
+  
   }
   return (
     <div class="contact-wrapper">
@@ -15,18 +26,19 @@ alert("Fill the foam")
      
       <div className="contact-form">
         <h2>Send Us a Message</h2>
+        {error && <p className="error-message">{error}</p>}
         <form action="#" method="POST">
           <div className="form-group">
             <label htmlFor="name">Full Name</label>
-            <input type="text" id="name" name="name" placeholder="Your full name" required />
+            <input type="text" id="name" name="name" placeholder="Your full name" value={formdata.name}  onChange={handleinput}  required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" name="email" placeholder="Your email" required />
+            <input type="email" id="email" name="email" placeholder="Your email" value={formdata.email} onChange={handleinput}  required />
           </div>
           <div className="form-group">
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" rows="5" placeholder="Your message" required></textarea>
+            <textarea id="message" name="message" rows="5" placeholder="Your message" value={formdata.message}  onChange={handleinput} required></textarea>
           </div>
           <button type="submit" className="submit-btn" onClick={submitbutton}>Submit</button>
         </form>
